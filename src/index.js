5ad5c0234d289e338fad3094fdf5ff3d1bdedc46
Tristan Bennett <tristan.bennett@ruralsource.com>
2018-05-29 09:46:29 -0600

    updating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {render} from 'react-dom';
+import ReactDOM, {render}  from 'react-dom';
 import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 //main app
