431ee8a29b76171c9dfbf9bc20fc8b9cf99ff84a
Tristan Bennett <tristan.bennett@ruralsource.com>
2018-05-29 09:27:27 -0600

    added sortable containers

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+
+const SortableItem = SortableElement(({value}) =>
+  <li>{value}</li>
+);
+
+const SortableList = SortableContainer(({items}) => {
+  return (
+    <ul>
+      {items.map((value, index) => (
+        <SortableItem key={`item-${index}`} index={index} value={value} />
+      ))}
+    </ul>
+  );
+});
+
 
 export default class TodoApp extends Component {
+
+	state = {
+		items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+	  };
+	  onSortEnd = ({oldIndex, newIndex}) => {
+		this.setState({
+		  items: arrayMove(this.state.items, oldIndex, newIndex),
+		});
+	  };
+
 	render() {
-		return (
-			<div className="vertical-line">
-				<h1>This will be a Drag and Drop Todo</h1>
-				<ol>
-					<li>Implement state</li>
-					<li>Render a task</li>
-					<li>Delete a task</li>
-					<li>Start Drag and Drop</li>
-				</ol>
-			</div>
-			
-		);
+		return <SortableList items={this.state.items} onSortEnd={this.onSortEnd} />;
+		
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 //main app
 import TodoApp from './TodoApp';
