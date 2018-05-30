d03ffbef5a45137066455d0f1ccdd3ec0ae89a39
Tristan Bennett <tristan.bennett@ruralsource.com>
2018-05-30 12:52:28 -0600

    made first column of tasks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -28,32 +28,21 @@ export default class TodoApp extends Component {
 	render() {
 		return (
 			<div className="container">
+				<h5>Todo Count: <span className="badge">{this.state.todos.length}</span></h5>
+				
 				<div className="row">
-					<h4>Todo Count: <span className="badge">{this.state.todos.length}</span></h4>
-					<div className="col-sm">
-							<ul className="list-group">
-											{this.state.todos.map((todo, index) =>
-												<li className="list-group-item" key="{index}">
-														<h4 className="list-group-item-heading">{todo.todoTitle}</h4>
-														<p>{todo.todoDescription}</p>
-												</li>
-								)}
-							</ul>
+					<div className="col-4">
+						<ul className="list-group">
+										{this.state.todos.map((todo, index) =>
+										
+											<li className="list-group-item" key="{index}">
+													<h4 className="list-group-item-heading">{todo.todoTitle}</h4>
+													<p>{todo.todoDescription}</p>
+											</li>
+										
+							)}
+						</ul>
 					</div>
-
-					</div>
-
-					
-					<div className="row">
-								<div className="col">
-									<p>hellow there!</p>
-								</div>
-								<div className="col">
-									<p>hellow there!</p>
-								</div>
-								<div className="col">
-									<p>hellow there!</p>
-								</div>
 				</div>
 
 			</div>
