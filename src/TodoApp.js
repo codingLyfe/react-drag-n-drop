7c73e070d5ba5a86e1ab17849c9b8bfa555241f3
Tristan Bennett <tristan.bennett@ruralsource.com>
2018-08-07 11:59:13 -0600

    Added more columns and dried up the code a bit

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -24,9 +24,14 @@ let todos = [
 		todoDescription: 'It should be CURRENT',
 		lane: 'current', 
 		id: '4'
+	},
+	{
+		todoTitle: 'Hopefully  Done',
+		  todoDescription: 'It should be completed',
+		  lane: 'complete', 
+		  id: '5'
 	}
 ];
-
 
 export default class TodoApp extends Component {
 
@@ -37,42 +42,86 @@ export default class TodoApp extends Component {
 			todos,
 			currentTodos: [],
 			futureTodos: [],
-			repeatTodos: []
+			repeatTodos: [],
+			completedTodos: []
 		};
 
-		this.setLane1 = this.setLane1.bind(this); 
+		this.setLanes = this.setLanes.bind(this);
 	}
 
 	componentDidMount() {
-		window.addEventListener('load', this.setLane1);
-		window.addEventListener('mouseup', this.setLane1);
+		window.addEventListener('load', this.setLanes);
+		window.addEventListener('mouseup', this.setLanes);
 	}
 
-	setLane1() {
-		
-		let newCurrentTodos =[];
+	setLanes() {
+
+		let newFutureTodos = [];
+		let newCurrentTodos = [];
+		let newRepeatTodos = [];
+		let newCompletedTodos = [];
+		let previousFutureTodos = this.state.futureTodos.slice();
 		let previousCurrentTodos = this.state.currentTodos.slice();
+		let previousRepeatTodos = this.state.repeatTodos.slice();
+		let previousCompletedTodos = this.state.completedTodos.slice();
 
-		console.log(newCurrentTodos);
-		console.log(previousCurrentTodos);
-
-		
-			this.state.todos.forEach(function(todo) {
-				if (todo.lane === 'current') {
-					newCurrentTodos.push(todo);	
-				}
-			})
-
-			if ((this.state.currentTodos.length !== previousCurrentTodos.length) || (this.state.currentTodos.length === 0)) {	
-				this.setState ({
-					currentTodos: newCurrentTodos
-				})
-				
+		// loops through Todos and places them in the correct array
+		this.state.todos.forEach(function(todo) {
+			if (todo.lane ===	 'repeat') {
+				newRepeatTodos.push(todo);
 			}
-			console.log(this.state);
-			return this.state;
-	}
+			
+			if (todo.lane === 'current') {
+				newCurrentTodos.push(todo);	
+			}
 
+			if (todo.lane === 'complete') {
+				newCompletedTodos.push(todo);
+			}
+			if (todo.lane === 'future') {
+				newFutureTodos.push(todo);
+			}
+
+		})
+
+		/**
+		 * Takes respective arrays and sets a new state depending on the colomn
+		 * 
+		 * This allows for the drag and drop to re-build the columns
+		 */
+		
+		// lane 0
+		if ((this.state.futureTodos !== previousFutureTodos) || (this.state.futureTodos.length === 0)) {	
+			this.setState ({
+				futureTodos: newFutureTodos
+			})
+			
+		}
+		// lane 1
+		if ((this.state.repeatTodos !== previousRepeatTodos) || (this.state.repeatTodos.length === 0)) {	
+			this.setState ({
+				repeatTodos: newRepeatTodos
+			})
+			
+		}
+
+		// lane 2
+		if ((this.state.currentTodos !== previousCurrentTodos) || (this.state.currentTodos.length === 0)) {	
+			this.setState ({
+				currentTodos: newCurrentTodos
+			})
+			
+		}
+
+		// lane 3
+		if ((this.state.completedTodos !== previousCompletedTodos) || (this.state.completedTodos.length === 0)) {
+			this.setState ({
+				completedTodos: newCompletedTodos
+			})
+		}
+
+		return this.state;		
+	}
 
 	render() {
 		
@@ -81,32 +130,57 @@ export default class TodoApp extends Component {
 				
 					<h5>Total Todo Count: <span className="badge">{this.state.todos.length}</span></h5>
 					
+
+
 					<div className="row">
+
 						<div className="col">
-						<p>Current Todos: {this.state.currentTodos.length}</p>
+							<p>Future Todos: {this.state.futureTodos.length}</p>
 							<ul className="list-group">
-								{this.state.currentTodos.map((currentTodo, index) =>
-									<li className="list-group-item" key="{index}">
+								{this.state.futureTodos.map((futureTodo, index2) =>
+									<li className="list-group-item" key="{index4}">
+											<h5 className="list-group-item-heading">{futureTodo.todoTitle}</h5>
+											<p>{futureTodo.todoDescription}</p>
+									</li>
+								)}
+							</ul>
+						</div>
+					
+						<div className="col">
+							<p>Current Todos: {this.state.currentTodos.length}</p>
+							<ul className="list-group">
+								{this.state.currentTodos.map((currentTodo, index2) =>
+									<li className="list-group-item" key="{index2}">
 											<h5 className="list-group-item-heading">{currentTodo.todoTitle}</h5>
 											<p>{currentTodo.todoDescription}</p>
 									</li>
 								)}
 							</ul>
-					</div>
-
-					<div className="col">
-						<p>Todos: {this.state.todos.length}</p>
-							<ul className="list-group">
-											{this.state.todos.map((todo, index) =>
-												<li className="list-group-item" key="{index}">
-														<h5 className="list-group-item-heading">{todo.todoTitle}</h5>
-														<p>{todo.todoDescription}</p>
-												</li>
-								)}
-							</ul>
 						</div>
-						
-						<div className="col"></div>
+
+						<div className="col">
+							<p>Repeat Todos: {this.state.repeatTodos.length}</p>
+								<ul className="list-group">
+												{this.state.repeatTodos.map((repeatTodo, index) =>
+													<li className="list-group-item" key="{index}">
+															<h5 className="list-group-item-heading">{repeatTodo.todoTitle}</h5>
+															<p>{repeatTodo.todoDescription}</p>
+													</li>
+									)}
+								</ul>
+						</div>
+
+						<div className="col">
+								<p>Completed: {this.state.completedTodos.length}</p>
+								<ul className="list-group">
+									{this.state.completedTodos.map((completedTodo, index3) =>
+										<li className="list-group-item" key="{index3}">
+											<h5 className="list-group-item-heading">{completedTodo.todoTitle}</h5>
+											<p>{completedTodo.todoDescription}</p>
+										</li>
+									)}
+								</ul>
+						</div>
 
 					</div>
 	
